5ae65410f9662dd3214d4887c404baa4d38b2d25
Patryk <patryk.bogdanski95@gmail.com>
2022-07-05 17:11:01 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IndividualMovie from './movies/IndividualMovie';
+import { landingPageDTO, movieDTO } from './movies/movies.model';
+import MoviesList from './movies/MoviesList';
 
 function App() {
+  const [movies, setMovies] = useState<landingPageDTO>({});
+
+  useEffect(() => {
+    const timerId = setTimeout(
+      () => {
+        setMovies({
+          inTheaters: [{
+            id: 1,
+            title: 'Star Wars New Hope',
+            poster: 'https://m.media-amazon.com/images/I/81w4kfIpSjL.jpg'
+          }, {
+            id: 2,
+            title: 'Star Wars Empire Strike Back',
+            poster: 'https://a.allegroimg.com/original/11397c/8a639dee4d2bacf8a041686caf15/STAR-WARS-EPISODE-V-THE-EMPIRE-STRIKES-BACK-DVD'
+          }],
+          upcoming: [
+            {
+              id: 1,
+              title: 'Obi-Wan Kenobi',
+              poster: 'https://cdn1.naekranie.pl/wp-content/uploads/2022%2F05%2FFR6mcs0XMAIQI-H_627278d335c84.jpeg'
+            }, {
+              id: 2,
+              title: 'The Mandalorian',
+              poster: 'https://static.posters.cz/image/750/plakaty/star-wars-the-mandalorian-nightfall-i103406.jpg'
+            }
+          ]
+        })
+      }, 1000);
+
+    return () => clearTimeout(timerId);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>In Theaters</h3>
+      <MoviesList movies={movies.inTheaters} />
+      <h3>Upcoming</h3>
+      <MoviesList movies={movies.upcoming} />
+    </>
   );
 }
 
---
+++ src/utils/Loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading(){
+    return (
+        <img alt="loading" src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif'/>
+    )
+}
