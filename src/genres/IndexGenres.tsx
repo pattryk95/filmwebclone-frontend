bb6d3249f53ea4d7f00bb2c37bc7479afb17eb41
Patryk <patryk.bogdanski95@gmail.com>
2022-07-20 23:22:10 +0200

    Add Axios to project

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,7 +1,22 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { genreDTO } from "./genres.model";
 
-export default function IndexGenres(){
-    return(
+export default function IndexGenres()
+{
+
+    useEffect(() =>
+    {
+        axios.get('https://localhost:7161/api/genres')
+            .then((response: AxiosResponse<genreDTO[]>) =>
+            {
+                console.log(response.data);
+                console.log("Test1");
+            })
+    }, [])
+
+    return (
         <>
             <h3>Genres</h3>
             <Link className="btn btn-primary" to="/genres/create">Create Genres</Link>
