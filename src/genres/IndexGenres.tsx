3338d060198d742ca8f42c5c25c4a49d75b2c94c
Patryk <patryk.bogdanski95@gmail.com>
2022-07-31 00:28:27 +0200

    Add displaying genres in IndexGenres

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,19 +1,21 @@
 import axios, { AxiosResponse } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { urlGenres } from "../endpoints";
+import Button from "../utils/Button";
+import GenericList from "../utils/GenericList";
 import { genreDTO } from "./genres.model";
 
 export default function IndexGenres()
 {
+const [genres, setGenres] = useState<genreDTO[]>();
 
     useEffect(() =>
     {
         axios.get(urlGenres)
             .then((response: AxiosResponse<genreDTO[]>) =>
             {
-                console.log(response.data);
-                console.log("Test1");
+                setGenres(response.data);
             })
     }, [])
 
@@ -21,7 +23,29 @@ export default function IndexGenres()
         <>
             <h3>Genres</h3>
             <Link className="btn btn-primary" to="/genres/create">Create Genres</Link>
-
+            <GenericList list={genres}>
+                <table className="table table-striped">
+                    <thead>
+                        <th></th>
+                        <th>Name</th>
+                    </thead>
+                    <tbody>
+                       {genres?.map(genre=>
+                        
+                        <tr key={genre.id}>
+                            <td>
+                                <Link className="btn btn-success" 
+                                to={`/genres/${genre.id}`}>Edit</Link>
+                                <Button className="btn btn-danger">Delete</Button>
+                            </td>
+                            <td>
+                                {genre.name}
+                            </td>
+                        </tr>
+                        )} 
+                    </tbody>
+                </table>
+            </GenericList>
         </>
     )
 }
