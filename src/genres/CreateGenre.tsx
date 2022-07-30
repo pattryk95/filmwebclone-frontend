8aca5cb522700cc22606f16b0c6d94bf93f0aab2
Patryk <patryk.bogdanski95@gmail.com>
2022-07-30 22:06:51 +0200

    Add DisplayError for CreateGenre

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,32 +1,33 @@
 import axios from "axios";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { urlGenres } from "../endpoints";
+import DisplayErrors from "../utils/DisplayErrors";
 import GenreForm from "./GenreForm";
 import { genreCreationDTO } from "./genres.model";
 
-export default function CreateGenre()
-{
+export default function CreateGenre() {
   const history = useHistory();
+  const [errors, setErrors] = useState<string[]>([]);
 
-  async function create(genre: genreCreationDTO)
-  {
-    try
-    {
+  async function create(genre: genreCreationDTO) {
+    try {
       await axios.post(urlGenres, genre)
       history.push('/genres');
-    } catch (error)
-    {
-      console.error(error)
+    } catch (error : any) {
+      if (error && error.response) {
+        setErrors(error.response.data);
+      }
     }
   }
 
   return (
     <>
       <h3>Create Genre</h3>
+      <DisplayErrors errors={errors}/>
       <GenreForm
         model={{ name: "" }}
-        onSubmit={async (value) =>
-        {
+        onSubmit={async (value) => {
           await create(value);
         }}
       />
