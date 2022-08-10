51f80932b647b01153fe888a5d339f94aa80a35a
Patryk <patryk.bogdanski95@gmail.com>
2022-08-10 20:08:42 +0200

    Add Creating MovieTheaters

--- src/movietheaters/CreateMovieTheaters.tsx
+++ src/movietheaters/CreateMovieTheaters.tsx
@@ -1,12 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { urlMovieTheater } from "../endpoints";
+import DisplayErrors from "../utils/DisplayErrors";
+import { movieTheaterCreationDTO } from "./movieTheater.model";
 import MovieTheaterForm from "./MovieTheaterForm";
 
-export default function CreateMovieTheater(){
+export default function CreateMovieTheater()
+{
+    const history = useHistory();
+    const [errors, setErrors] = useState<string[]>([]);
+    async function create(movieTheater: movieTheaterCreationDTO)
+    {
+        try
+        {
+            await axios.post(urlMovieTheater, movieTheater);
+            history.push("/movietheaters");
+        } catch (error: any)
+        {
+            if (error && error.response)
+            {
+                setErrors(error.response.data);
+            }
+        }
+    }
+
     return (
         <>
             <h3>Create Movie Theater</h3>
+            <DisplayErrors errors={errors} />
             <MovieTheaterForm
-                model={{name: ''}}
-                onSubmit={values => console.log(values)}
+                model={{ name: '' }}
+                onSubmit={async values => await create(values)}
             />
         </>
     )
