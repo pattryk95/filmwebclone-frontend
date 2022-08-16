29fa8e728074f9cdaa7fa9b029c3d3e0a7a588e7
Patryk <patryk.bogdanski95@gmail.com>
2022-08-16 15:05:53 +0200

    Improve selecting actors

--- src/forms/TypeAheadActors.tsx
+++ src/forms/TypeAheadActors.tsx
@@ -1,25 +1,30 @@
+import axios, { AxiosResponse } from "axios";
 import { useState } from "react";
-import { Typeahead } from "react-bootstrap-typeahead";
+import { AsyncTypeahead } from "react-bootstrap-typeahead";
 import { ReactElement } from "react-markdown/lib/react-markdown";
 import { actorMovieDTO } from "../actors/actors.model";
+import { urlActors } from "../endpoints";
 
 export default function TypeAheadActors(props: typeAheadActorsProps)
 {
-    const actors: actorMovieDTO[] = [
-        {
-            id: 1, name: 'Ewan McGregor', character: 'Obi-Wan Kenobi', picture: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQIIUCug0cluUJPxTvs2BjRSWXL5civuHUqbflF1cxc5vx47ZHS'
-        },
-        {
-            id: 2, name: 'Hayden Christensen', character: 'Anakin Skywalker', picture: 'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcT3M46awtwGAtdCCBB1tXSG0FD2c5KIAXjBDwwX-FmR8vbpFl64gRy6qexv_xI5PkW3'
-        },
-        {
-            id: 3, name: 'Mark Hamill', character: 'Luke Skywalker', picture: 'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQts8lFduAM79i8ZdB1fAgpITZCZBxyFx5IXzfGA2dDq6zcaKzdDVm4eDA7D3YBIT3n'
-        }
-    ]
+    const [actors, setActors] = useState<actorMovieDTO[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const selected: actorMovieDTO[] = [];
 
     const [draggedElement, setDraggedElement] = useState<actorMovieDTO | undefined>(undefined);
+
+    function handleSearch(query: string)
+    {
+        setIsLoading(true);
+
+        axios.get(`${urlActors}/searchByName/${query}`)
+            .then((response: AxiosResponse<actorMovieDTO[]>) =>
+            {
+                setActors(response.data);
+                setIsLoading(false);
+            })
+    }
 
     function handleDragStart(actor: actorMovieDTO)
     {
@@ -45,19 +50,21 @@ export default function TypeAheadActors(props: typeAheadActorsProps)
     return (
         <div className="mb-3">
             <label>{props.displayName}</label>
-            <Typeahead
+            <AsyncTypeahead
                 id="typeahead"
                 onChange={actors =>
                 {
                     if (props.actors.findIndex(x => x.id === actors[0].id) === -1)
                     {
+                        actors[0].character = ''
                         props.onAdd([...props.actors, actors[0]]);
                     }
-                    console.log(actors);
                 }}
                 options={actors}
                 labelKey={actor => actor.name}
-                filterBy={['name']}
+                filterBy={() => true}
+                isLoading={isLoading}
+                onSearch={handleSearch}
                 placeholder="Write the name of the actor..."
                 minLength={1}
                 flip={true}
