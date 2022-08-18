145e5f49475800ab050e925247639a5ce194775a
Patryk <patryk.bogdanski95@gmail.com>
2022-08-18 19:16:39 +0200

    Revert "Add GetAll Action fill landing page"
    This reverts commit 28d7027ce8c45782d6a91c10846f3d085dd8b7f1.

--- src/movies/IndividualMovie.tsx
+++ src/movies/IndividualMovie.tsx
@@ -1,19 +1,17 @@
 import { movieDTO } from "./movies.model";
 import css from './IndividualMovie.module.css'
-import { Link } from "react-router-dom";
 
-export default function IndividualMovie(props: movieDTO)
-{
+export default function IndividualMovie(props: movieDTO) {
 
     const buildLink = () => `/movie/${props.id}`
 
     return (
         <div className={css.div}>
-            <Link to={buildLink()}>
+            <a href={buildLink()}>
                 <img alt="Poster" src={props.poster}></img>
-            </Link>
+            </a>
             <p>
-                <Link to={buildLink()}>{props.title}</Link>
+                <a href={buildLink()}>{props.title}</a>
             </p>
         </div>
     )
--- src/movies/LandingPage.tsx
+++ src/movies/LandingPage.tsx
@@ -1,6 +1,4 @@
-import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
-import { urlMovies } from "../endpoints";
 import { landingPageDTO } from "./movies.model";
 import MoviesList from "./MoviesList";
 
@@ -10,10 +8,7 @@ export default function LandingPage()
 
   useEffect(() =>
   {
-    axios.get(urlMovies).then((response: AxiosResponse<landingPageDTO>) =>
-    {
-      setMovies(response.data);
-    })
+
   }, []);
   return (
     <>
