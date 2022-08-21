10b73ae86c731ca87c27dd68c235d6de621443ef
Patryk <patryk.bogdanski95@gmail.com>
2022-08-21 19:23:39 +0200

    Add movies filtering

--- src/movies/FilterMovies.tsx
+++ src/movies/FilterMovies.tsx
@@ -1,107 +1,152 @@
+import axios, { AxiosResponse } from "axios";
 import { Field, Form, Formik } from "formik";
+import { useEffect, useState } from "react";
+import { urlGenres, urlMovies } from "../endpoints";
 import { genreDTO } from "../genres/genres.model";
 import Button from "../utils/Button";
+import { movieDTO } from "./movies.model";
+import MoviesList from "./MoviesList";
 
-export default function FilterMovies() {
+export default function FilterMovies()
+{
   const initialValues: filterMoviesForm = {
-    title: "",
+    title: '',
     genreId: 0,
-    upcomingReleases: false,
+    upcoming: false,
     inTheaters: false,
+    page: 1,
+    recordsPerPage: 10
   };
 
-  const genres: genreDTO[] = [
-    { id: 1, name: "Drama" },
-    { id: 2, name: "Comedy" },
-    { id: 3, name: "Sci-Fi" },
-  ];
+  const [genres, setGenres] = useState<genreDTO[]>([]);
+  const [movies, setMovies] = useState<movieDTO[]>([]);
+
+  useEffect(() =>
+  {
+    axios.get(`${urlGenres}/all`)
+      .then((response: AxiosResponse<genreDTO[]>) =>
+      {
+        setGenres(response.data);
+      })
+  }, []);
+
+  useEffect(() =>
+  {
+    searchMovies(initialValues);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function searchMovies(values: filterMoviesForm)
+  {
+    axios.get(`${urlMovies}/filter`, { params: values })
+      .then((response: AxiosResponse<movieDTO[]>) =>
+      {
+        setMovies(response.data);
+      })
+  }
 
   return (
     <>
       <h3>Filter Movies</h3>
       <Formik
         initialValues={initialValues}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={values =>
+        {
+          values.page = 1;
+          searchMovies(values);
+        }}
       >
-        {(formikProps) => (
-          <Form>
-            <div className="row gx-3 align-items-center">
-              <div className="col-auto">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="title"
-                  placeholder="Title of the movie"
-                  {...formikProps.getFieldProps("title")}
-                />
-              </div>
-              <div className="col-auto">
-                <select
-                  className="form-select"
-                  {...formikProps.getFieldProps("genreId")}
-                >
-                  <option value="0">--Choose a genre--</option>
-                  {genres.map((genre) => (
-                    <option key={genre.id} value={genre.id}>
-                      {genre.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="col-auto">
-                <div className="form-check">
-                  <Field
-                    className="form-check-input"
-                    id="upcomingReleases"
-                    name="upcomingReleases"
-                    type="checkbox"
+        {(formikProps) =>
+        (
+          <>
+            <Form>
+              <div className="row gx-3 align-items-center mb-3">
+                <div className="col-auto">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    placeholder="Title of the movie"
+                    {...formikProps.getFieldProps("title")}
                   />
-                  <label
-                    className="form-check-label"
-                    htmlFor="upcomingReleases"
+                </div>
+                <div className="col-auto">
+                  <select
+                    className="form-select"
+                    {...formikProps.getFieldProps("genreId")}
                   >
-                    Upcoming Releases
-                  </label>
+                    <option value="0">--Choose a genre--</option>
+                    {genres.map((genre) => (
+                      <option key={genre.id} value={genre.id}>
+                        {genre.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="col-auto">
+                  <div className="form-check">
+                    <Field
+                      className="form-check-input"
+                      id="upcoming"
+                      name="upcoming"
+                      type="checkbox"
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="upcoming"
+                    >
+                      Upcoming Releases
+                    </label>
+                  </div>
+                </div>
+                <div className="col-auto">
+                  <div className="form-check">
+                    <Field
+                      className="form-check-input"
+                      id="inTheaters"
+                      name="inTheaters"
+                      type="checkbox"
+                    />
+                    <label className="form-check-label" htmlFor="inTheaters">
+                      In Theaters
+                    </label>
+                  </div>
+                </div>
+                <div className="col-auto">
+                  <Button
+                    className="btn btn-primary"
+                    onClick={() => formikProps.submitForm()}
+                  >
+                    Filter
+                  </Button>
+                  <Button
+                    className="btn btn-danger ms-3"
+                    onClick={() =>
+                    {
+                      formikProps.setValues(initialValues);
+                      searchMovies(initialValues);
+                    }}
+                  >
+                    Clear
+                  </Button>
                 </div>
               </div>
-              <div className="col-auto">
-                <div className="form-check">
-                  <Field
-                    className="form-check-input"
-                    id="inTheaters"
-                    name="inTheaters"
-                    type="checkbox"
-                  />
-                  <label className="form-check-label" htmlFor="inTheaters">
-                    In Theaters
-                  </label>
-                </div>
-              </div>
-              <div className="col-auto">
-                <Button
-                  className="btn btn-primary"
-                  onClick={() => formikProps.submitForm}
-                >
-                  Filter
-                </Button>
-                <Button
-                  className="btn btn-danger ms-3"
-                  onClick={() => formikProps.setValues(initialValues)}
-                >
-                  Clear
-                </Button>
-              </div>
-            </div>
-          </Form>
+            </Form>
+            <MoviesList movies={movies} />
+          </>
         )}
       </Formik>
     </>
   );
 }
 
-interface filterMoviesForm {
+interface filterMoviesForm
+{
   title: string;
   genreId: number;
-  upcomingReleases: boolean;
+  upcoming: boolean;
   inTheaters: boolean;
+  page: number;
+  recordsPerPage: number;
 }
